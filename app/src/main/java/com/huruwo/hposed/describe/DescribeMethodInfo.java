57ff82841d9e9cefacfe4868b7d71a1eadf5d371
liuw <1458476478@qq.com>
2022-01-13 10:28:27 +0800

    -- add DescriptionFindClass

--- app/src/main/java/com/huruwo/hposed/describe/DescribeMethodInfo.java
+++
@@ -1,8 +0,0 @@
-package com.huruwo.hposed.describe;
-
-public class DescribeMethodInfo {
-    private Class[] inCls;
-    private int argCount;
-    private Class[] argCls;
-    private Class returnCls;
-}
